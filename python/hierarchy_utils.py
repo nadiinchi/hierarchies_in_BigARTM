69c1c947da371967b200d5d8a8c20887c5c97af0
nadiinchi <nadiinchi@gmail.com>
2016-07-26 18:52:57 +0300

    Added hARTM
    Added hARTM and removed ignore_cache_theta from get_psi method

--- python/hierarchy_utils.py
+++ python/hierarchy_utils.py
@@ -2,10 +2,234 @@ import artm
 import uuid
 import copy
 import numpy as np
+import os.path
+import os
+import pickle
+import glob
+import warnings
+
+class hARTM:
+    def __init__(self, num_processors=None, class_ids=None,
+                 scores=None, regularizers=None, num_document_passes=10, reuse_theta=False,
+                 dictionary=None, cache_theta=False, theta_columns_naming='id', seed=-1):
+        self._common_models_args = {"num_processors": num_processors,
+                                   "class_ids": class_ids,
+                                   "scores": scores,
+                                   "regularizers": regularizers,
+                                   "num_document_passes": num_document_passes,
+                                   "reuse_theta": reuse_theta,
+                                   "dictionary": dictionary,
+                                   "cache_theta": cache_theta,
+                                   "theta_columns_naming": theta_columns_naming}
+        if seed > 0:
+            self._seed = seed
+        else:
+            seed = 321
+        self._levels = []
+        
+    # ========== PROPERTIES ==========
+    @property
+    def num_processors(self):
+        return self._common_models_args["num_processors"]
+
+    @property
+    def cache_theta(self):
+        return self._common_models_args["cache_theta"]
+
+    @property
+    def reuse_theta(self):
+        return self._common_models_args["reuse_theta"]
+
+    @property
+    def num_document_passes(self):
+        return self._common_models_args["num_document_passes"]
+
+    @property
+    def theta_columns_naming(self):
+        return self._common_models_args["theta_columns_naming"]
+
+    @property
+    def class_ids(self):
+        return self._common_models_args["class_ids"]
+
+    @property
+    def regularizers(self):
+        return self._common_models_args["regularizers"]
+
+    @property
+    def scores(self):
+        return self._common_models_args["scores"]
+        
+    @property
+    def dictionary(self):
+        return self._common_models_args["dictionary"]
+
+    @property
+    def seed(self):
+        return self._see
+
+    @property
+    def library_version(self):
+        """
+        :Description: the version of BigARTM library in a MAJOR.MINOR.PATCH format
+        """
+        return self._lib.version()
+
+    # ========== SETTERS ==========
+    @num_processors.setter
+    def num_processors(self, num_processors):
+        if num_processors <= 0 or not isinstance(num_processors, int):
+            raise IOError('Number of processors should be a positive integer')
+        else:
+            for level in self._levels:
+                level.num_processors = num_processors
+            self._common_models_args["num_processors"] = num_processors
+
+    @cache_theta.setter
+    def cache_theta(self, cache_theta):
+        if not isinstance(cache_theta, bool):
+            raise IOError('cache_theta should be bool')
+        else:
+            for level in self._levels:
+                level.cache_theta = cache_theta
+            self._common_models_args["cache_theta"] = cache_theta
+
+    @reuse_theta.setter
+    def reuse_theta(self, reuse_theta):
+        if not isinstance(reuse_theta, bool):
+            raise IOError('reuse_theta should be bool')
+        else:
+            for level in self._levels:
+                level.reuse_theta = reuse_theta
+            self._common_models_args["reuse_theta"] = reuse_theta
+
+    @num_document_passes.setter
+    def num_document_passes(self, num_document_passes):
+        if num_document_passes <= 0 or not isinstance(num_document_passes, int):
+            raise IOError('Number of passes through document should be a positive integer')
+        else:
+            for level in self._levels:
+                level.num_document_passes = num_document_passes
+            self._common_models_args["num_document_passes"] = num_document_passes
+
+    @theta_columns_naming.setter
+    def theta_columns_naming(self, theta_columns_naming):
+        if theta_columns_naming not in ['id', 'title']:
+            raise IOError('theta_columns_naming should be either "id" or "title"')
+        else:
+            for level in self._levels:
+                level.theta_columns_naming = theta_columns_naming
+            self._common_models_args["theta_columns_naming"] = theta_columns_naming
+
+    @class_ids.setter
+    def class_ids(self, class_ids):
+        if len(class_ids) < 0:
+            raise IOError('Number of (class_id, class_weight) pairs should be non-negative')
+        else:
+            for level in self._levels:
+                level.class_ids = class_ids
+            self._common_models_args["class_ids"] = class_ids
+            
+    @scores.setter
+    def scores(self, scores):
+        if not isinstance(scores, list):
+            raise IOError('scores should be a list')
+        else:
+           
+            self._common_models_args["scores"] = scores
+            
+    @regularizers.setter
+    def regularizers(self, regularizers):
+        if not isinstance(regularizers, list):
+            raise IOError('scores should be a list')
+        else:
+            self._common_models_args["regularizers"] = regularizers
+            
+    @dictionary.setter
+    def dictionary(self, dictionary):
+        self._common_models_args["dictionary"] = dictionary
+
+    @seed.setter
+    def seed(self, seed):
+        if seed < 0 or not isinstance(seed, int):
+            raise IOError('Random seed should be a positive integer')
+        else:
+            self._seed = seed
+            
+    # ========== METHODS ==========
+    def _get_seed(self, level_idx):
+        np.random.seed(self._seed)
+        return np.random.randint(10000, size=level_idx+1)[-1]
+        
+    def add_level(self, num_topics=None, topic_names=None, parent_level_weight=1,
+                  tmp_files_path=""):
+        if len(self._levels) and num_topics <= self._levels[-1].num_topics:
+            warnings.warn("Adding level with num_topics = %s less or equal than parent level's num_topics = %s"%\
+                          (num_topics, self._levels[-1].num_topics))
+        level_idx = len(self._levels)
+        if not len(self._levels):
+            self._levels.append(artm.ARTM(num_topics=num_topics,
+                                          topic_names=topic_names,
+                                          seed=self._get_seed(level_idx),
+                                          **self._common_models_args))
+        else:
+            self._levels.append(ARTM_Level(parent_model=self._levels[-1],
+                                           phi_batch_weight=parent_level_weight,
+                                           phi_batch_path=tmp_files_path,
+                                           num_topics=num_topics,
+                                           topic_names=topic_names,
+                                           seed=self._get_seed(level_idx),
+                                           **self._common_models_args))
+        return self._levels[-1]
+                                           
+    def del_level(self, level_idx):
+        for _ in xrange(level_idx, len(self._levels)):
+            del self._levels[-1]
+            
+    def get_level(self, level_idx):
+        return self._levels[level_idx]
+        
+        
+    def save(self, path, model_name='p_wt'):
+        for file in glob.glob(os.path.join(path, "*")):
+            os.remove(file)
+        for level_idx, level in enumerate(self._levels):
+            level.save(os.path.join(path, "level"+str(level_idx)+".model"))
+        info = {"parent_level_weight": [level.phi_batch_weight for level in self._levels[1:]],\
+                "tmp_files_path": [os.path.split(level.phi_batch_path)[0] for level in self._levels[1:]]}
+        with open(os.path.join(path, "info.dump"), "wb") as fout:
+            pickle.dump(info, fout)        
+            
+    def load(self, path):
+        info_filename = glob.glob(os.path.join(path, "info.dump"))
+        if len(info_filename) != 1:
+            raise ValueError("Given path is not hARTM safe")
+        with open(info_filename[0]) as fin:
+            info = pickle.load(fin)
+        model_filenames = glob.glob(os.path.join(path, "*.model"))
+        if len({len(info["parent_level_weight"])+1, len(info["tmp_files_path"])+1, len(model_filenames)}) > 1:
+            raise ValueError("Given path is not hARTM safe")
+        self._levels = []
+        for level_idx, filename in enumerate(sorted(model_filenames)):
+            if not len(self._levels):
+                model = artm.ARTM(num_topics=1,
+                                  seed=self._get_seed(level_idx),
+                                  **self._common_models_args)
+            else:
+                parent_level_weight = info["parent_level_weight"][level_idx-1]
+                tmp_files_path = info["tmp_files_path"][level_idx-1]
+                model = ARTM_Level(parent_model=self._levels[-1],
+                                   phi_batch_weight=parent_level_weight,
+                                   phi_batch_path=tmp_files_path,
+                                   num_topics=1,
+                                   seed=self._get_seed(level_idx),
+                                   **self._common_models_args)
+            model.load(filename)
+            self._levels.append(model)
 
 
 class ARTM_Level(artm.ARTM):
-    def __init__(self, parent_model, phi_batch_weight=1, phi_batch_path="", 
+    def __init__(self, parent_model, phi_batch_weight=1, phi_batch_path=".", 
                  *args, **kwargs):
         """
         :description: builds one hierarchy level that is usual topic model
@@ -26,7 +250,9 @@ class ARTM_Level(artm.ARTM):
         """
         self.parent_model = parent_model
         self.phi_batch_weight = phi_batch_weight
-        self.phi_batch_path = phi_batch_path + "phi.batch"
+        self._level = 1 if not "_level" in dir(parent_model) else parent_model._level + 1
+        self._name = "level" + str(self._level)
+        self.phi_batch_path = os.path.join(phi_batch_path, "phi"+str(self._level)+".batch")
         super(ARTM_Level, self).__init__(*args, **kwargs)
         self._create_parent_phi_batch()
         self._create_parent_phi_batch_vectorizer()
@@ -66,7 +292,7 @@ class ARTM_Level(artm.ARTM):
                     field.token_id.append(batch_dict[token])
                     field.token_weight.append(float(weight))
                     NNZ += weight
-        #self.parent_batch = batch
+        # self.parent_batch = batch
         with open(self.phi_batch_path, 'wb') as fout:
             fout.write(batch.SerializeToString())
             
@@ -118,20 +344,12 @@ class ARTM_Level(artm.ARTM):
         raise NotImplementedError("Fit_online method is not implemented in hARTM. Use fit_offline method.")
 
 
-    def get_psi(self, ignore_cache_theta=False):
+    def get_psi(self):
         """
         :returns: p(subtopic|topic) matrix
-        
-        :param ignore_cache_theta: if True, get_theta() method will not be used
         """
-        if self.cache_theta and not ignore_cache_theta:
-            current_columns_naming = self.theta_columns_naming
-            self.theta_columns_naming = "title"
-            psi = self.get_theta()[self.parent_model.topic_names]
-            self.theta_columns_naming = current_columns_naming
-        else:
-            current_columns_naming = self.theta_columns_naming
-            self.theta_columns_naming = "title"
-            psi = self.transform(self.phi_batch_vectorizer)
-            self.theta_columns_naming = current_columns_naming
+        current_columns_naming = self.theta_columns_naming
+        self.theta_columns_naming = "title"
+        psi = self.transform(self.phi_batch_vectorizer)
+        self.theta_columns_naming = current_columns_naming
         return psi
